f2e0903a673da02e7aebcc818cef9ee4bc62d4a7
Kay Elst <kayelst@gmail.com>
2016-03-21 10:31:55 +0100

    Merge remote-tracking branch 'origin/master'

--- client/templates/home/index.js
+++ client/templates/home/index.js
@@ -1,17 +1,45 @@
-/**
+/*
  * Created by Matthew on 28/03/2016.
  */
+import {Template} from 'meteor/templating';
+import {Playerslist} from '.../lib/collections/players.js';
+//Playerslist = new Mongo.Collection('Players');
 
 Template.homeIndex.events({
    'submit .registerForm': function(event) {
        event.preventDefault();
        Session.set("validSummonerName", false);
        Meteor.call("isValidSummonerName", event.target.summonerName.value.toLowerCase(), function (error, result) {
-           if (result) Session.set("validSummonerName", true);
+           if (result){
+           		Session.set("validSummonerName", true);
+           		
+           } 
            else Session.set("validSummonerName", false);
        });
-   }
+
+       Meteor.call("getSummonerIDByName", event.target.summonerName.value.toLowerCase(), function(error, result) {
+       		console.log("went in function");
+       		if (error){
+       			console.log("Error getting summoner ID");
+       		}
+       		else {
+       			console.log("Succes!" + result);
+       			Players.insert({
+       				"leagueID" : result
+       			});
+
+       		}
+		});
+   	}
 });
+
+Template.homeIndex.events({
+  'input #LeagueID' :function LeagueIDHandler(evt, template){
+    let LID = template.find('#LeagueID').value;
+    Playerslist.insert({'Name':'Thesmoggy', 'LeagueID': LID})
+    console(LID);
+  }
+})
 
 Template.registerHelper('validSummonerName',function(){
     return Session.get("validSummonerName");
