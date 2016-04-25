0ecd9e27a31502be759274be769c75ea7d55630d
Matthew Berkvens <matthew@berkvens.com>
2016-04-25 11:32:32 +0200

    settings page

--- client/scripts/register-setup.js
+++ client/scripts/register-setup.js
@@ -5,54 +5,71 @@
 Accounts.ui.config({
     requestPermissions: {},
     extraSignupFields: [{
-        fieldName: 'first-name',
-        fieldLabel: 'First name',
+        fieldName: 'username',
+        fieldLabel: 'Username',
         inputType: 'text',
         visible: true,
         validate: function(value, errorFunction) {
             if (!value) {
-                errorFunction("Please write your first name");
+                errorFunction("Please write your username");
                 return false;
             } else {
                 return true;
             }
         }
     }, {
-        fieldName: 'last-name',
-        fieldLabel: 'Last name',
+        fieldName: 'summoner-name',
+        fieldLabel: 'Summoner Name',
         inputType: 'text',
-        visible: true,
+        visible: true
     }, {
         fieldName: 'gender',
-        showFieldLabel: false,      // If true, fieldLabel will be shown before radio group
+        showFieldLabel: false,
         fieldLabel: 'Gender',
         inputType: 'radio',
-        radioLayout: 'vertical',    // It can be 'inline' or 'vertical'
-        data: [{                    // Array of radio options, all properties are required
-            id: 1,                  // id suffix of the radio element
-            label: 'Male',          // label for the radio element
-            value: 'm'              // value of the radio element, this will be saved.
+        radioLayout: 'vertical',
+        data: [{
+            id: 0,
+            label: 'Male',
+            value: 'm',
+            checked: 'checked'
         }, {
-            id: 2,
+            id: 1,
             label: 'Female',
             value: 'f',
-            checked: 'checked'
+
         }],
         visible: true
     }, {
-        fieldName: 'country',
-        fieldLabel: 'Country',
+        fieldName: 'server',
+        fieldLabel: 'Server',
         inputType: 'select',
         showFieldLabel: true,
-        empty: 'Please select your country of residence',
+        empty: 'Please select your server',
         data: [{
+            id: 0,
+            label: 'EUW',
+            value: 'EUW'
+        }, {
             id: 1,
-            label: 'United States',
-            value: 'us'
+            label: 'NA',
+            value: 'NA',
         }, {
             id: 2,
-            label: 'Spain',
-            value: 'es',
+            label: 'EUNE',
+            value: 'EUNE',
+        }, {
+            id: 2,
+            label: 'TR',
+            value: 'TR',
+        }, {
+            id: 4,
+            label: 'LAN',
+            value: 'LAN',
+        }, {
+            id: 5,
+            label: 'JAP',
+            value: 'JAP',
         }],
         visible: true
     }, {
--- client/templates/_partials/settings/settings.js
+++ client/templates/_partials/settings/settings.js
@@ -1,3 +1,9 @@
 /**
  * Created by Matthew on 25/04/2016.
  */
+
+Template.settings.helpers({
+    'radio_equals': function (x, y) {
+        return (x === y) ? {checked: 'checked'} : null;
+    }
+});
