e81a04e93d5d9f4cf3081ff54a4a3ed1628278eb
MikeMeyers2504 <mike_sloefke@hotmail.com>
2016-05-09 21:59:00 +0200

    working for the teams tab

---
+++ client/templates/_partials/teams/teams.js
@@ -0,0 +1,17 @@
+Template.homeTeams.events({
+   'submit .teamsearching': function(event) {
+       event.preventDefault();
+       var choicebox = document.getElementById("server").value;
+       var Name = document.getElementById("Name").value;
+       var Language = document.getElementById("Language").value;
+       var Microphone = document.getElementById("Microphone").value;
+       console.log(choicebox);
+       console.log(Name);
+       console.log(Language);
+       console.log(Microphone);
+   }
+});
+
+Template.registerHelper('',function(){
+    return Session.get("");
+});
