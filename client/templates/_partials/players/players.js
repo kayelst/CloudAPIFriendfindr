8cdc47d4f78aa11612b3faf7226d176ebcc967e2
MikeMeyers2504 <mike_sloefke@hotmail.com>
2016-05-28 20:10:36 +0200

    layout is al wat aangepast + het zoeken naar spelers werkt nu volledig

--- client/templates/_partials/players/players.js
+++ client/templates/_partials/players/players.js
@@ -1,40 +1,54 @@
 Template.homePlayers.helpers({
-  'languageCheck': function (value1) {
-    return value1.toLowerCase() === Session.get("Language").toLowerCase();
+  'languageCheck': function (value) {
+    if (!value || !Session.get("Language")) return true;
+    return value.toLowerCase() === Session.get("Language").toLowerCase();
   }
 });
 
 Template.homePlayers.helpers({
-  'microphoneCheck': function (value1) {
-    return value1.toLowerCase() === Session.get("Microphone").toLowerCase();
+  'microphoneCheck': function (value) {
+    if (value == undefined || Session.get("Microphone") == undefined) return true;
+    console.log(Session.get("Microphone") + " | "  + value + " : " + (value == Session.get("Microphone")));
+    return value == Session.get("Microphone");
   }
 });
 
 Template.homePlayers.helpers({
+<<<<<<< Updated upstream
   'serverCheck': function (value1) {
     return value1.toLowerCase() === Session.get("Server").toLowerCase();
       console.log("value is " + value1)
+=======
+  'serverCheck': function (value) {
+    if (!value || !Session.get("Server")) return true;
+    return value.toLowerCase() === Session.get("Server").toLowerCase();
+>>>>>>> Stashed changes
   }
 });
 
 Template.homePlayers.helpers({
-  'nameCheck': function (value1) {
-    return value1.toLowerCase() === Session.get("Name").toLowerCase();
+  'nameCheck': function (value) {
+    if (!value || !Session.get("Name")) return true;
+    return value.toLowerCase() === Session.get("Name").toLowerCase();
   }
 });
 
 Template.homePlayers.events({
    'submit .Playersearching': function(event) {
        event.preventDefault();
-       Session.set("Name", document.getElementById("Name").value);
-       Session.set("Microphone", document.getElementById("Microphone").value);
+       Session.set("Name", Name.value);
+       Session.set("Microphone", Microphone.checked);
        Session.set("Language", document.getElementById("Language").value);
        Session.set("Server", document.getElementById("Server").value);
+<<<<<<< Updated upstream
        //var choicebox = document.getElementById("server").value;
        //console.log(Server);
        //console.log(Name);
        /*console.log(Language);
        console.log(Microphone);*/
+=======
+>>>>>>> Stashed changes
    }
 });
+
 
