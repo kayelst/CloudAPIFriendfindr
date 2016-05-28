ab8f29d599524fe6ac46be127f8d517185967e8f
Matthew Berkvens <matthew@berkvens.com>
2016-05-28 22:36:42 +0200

    auto redirect and paypal premium account purchase

--- client/templates/_partials/paypal/paypal.js
+++ client/templates/_partials/paypal/paypal.js
@@ -4,13 +4,8 @@ Template.paypalCreditCardForm.events({
 
         var card_data = Template.paypalCreditCardForm.card_data();
 
-        //Probably a good idea to disable the submit button here to prevent multiple submissions.
-
-        Meteor.Paypal.purchase(card_data, {total: '2.00', currency: 'EUR'}, function(err, results){
-            if (err) console.error(err);
-            else console.log(results);
-
-            if (results.saved) Meteor.users.update(userId, { $set: { profile: { premium: true } } });
-        });
+        Meteor.call("paypal", card_data, function (err, result) {
+            if (result) Router.go('settings');
+        })
     }
 });
--- server/paypal.js
+++ server/paypal.js
@@ -1,4 +1,3 @@
-
 if (Meteor.settings.paypal.clientId != null && Meteor.settings.paypal.secret != null) {
     console.log("Using Paypal settings:\nClientId: " + Meteor.settings.paypal.clientId + "\nSecret: " + Meteor.settings.paypal.secret);
 
@@ -8,5 +7,23 @@ if (Meteor.settings.paypal.clientId != null && Meteor.settings.paypal.secret != 
         'client_id': Meteor.settings.paypal.clientId,
         'client_secret': Meteor.settings.paypal.secret
     });
+
+    Meteor.methods({
+        'paypal': function (card_data) {
+            var syncMethod = Meteor.wrapAsync(Meteor.Paypal.purchase);
+
+            try {
+                var result = syncMethod(card_data, {total: '2.00', currency: 'EUR'});
+                console.log(Meteor.userId());
+                if (result.saved) Meteor.users.update({_id: Meteor.userId()}, {$set: {profile: {premium: true}}}, {upsert: true});
+                return result.saved;
+            }
+            catch (ex) {
+                console.error(ex);
+                return false;
+            }
+            return true;
+        }
+    })
 }
 else console.error("Paypal settings not found, do you have a settings file named 'settings.json'? Start meteor with 'meteor run --settings settings.json' if you do.");
