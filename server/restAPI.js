63dfcefd572d115aada66d4669fa28527857ff2c
Matthew Berkvens <matthew@berkvens.com>
2016-05-29 17:36:44 +0200

    update api routes

--- server/restAPI.js
+++ server/restAPI.js
@@ -1,27 +1,51 @@
-JsonRoutes.add("get", "/players", function (req, res, next) {
+JsonRoutes.add("get", "/api/players", function (req, res, next) {
     JsonRoutes.sendResult(res, {
-        data: Meteor.users.find({}).fetch()
+        data: Meteor.users.find({}, {profile: 1})
     });
 });
 
-JsonRoutes.add("get", "/teams", function (req, res, next) {
+JsonRoutes.add("get", "/api/players/premium", function (req, res, next) {
+    var name = req.params.name;
+
+    JsonRoutes.sendResult(res, {
+        data: Meteor.users.find({profile: {premium: true}}, {profile: 1}).fetch()
+    });
+});
+
+JsonRoutes.add("get", "/api/players/:name", function (req, res, next) {
+    var name = req.params.name;
+
+    JsonRoutes.sendResult(res, {
+        data: Meteor.users.find({profile: {summonerName: name}}, {profile: 1})
+    });
+});
+
+JsonRoutes.add("get", "/api/teams", function (req, res, next) {
     JsonRoutes.sendResult(res, {
         data: Teams.find().fetch()
     });
 });
 
-JsonRoutes.add("get", "/players/:name", function (req, res, next) {
+JsonRoutes.add("get", "/api/teams/:name", function (req, res, next) {
     var name = req.params.name;
 
     JsonRoutes.sendResult(res, {
-        data: Meteor.users.find({ profile: { summonerName: name }}).fetch()
+        data: Teams.find({teamName: name}).fetch()
     });
 });
 
-JsonRoutes.add("get", "/teams/:name", function (req, res, next) {
-    var name = req.params.name;
+JsonRoutes.add("get", "/api/invites/team/:id", function (req, res, next) {
+    var id = req.params.id;
 
     JsonRoutes.sendResult(res, {
-        data: Teams.find({ teamName: name }).fetch()
+        data: Invites.find({teamId: id}).fetch()
+    });
+});
+
+JsonRoutes.add("get", "/api/invites/player/:id", function (req, res, next) {
+    var id = req.params.id;
+
+    JsonRoutes.sendResult(res, {
+        data: Invites.find({playerId: id}).fetch()
     });
 });
--- server/teams.js
+++ server/teams.js
@@ -51,6 +51,5 @@ Meteor.methods({
                 });
             }
         });
-
     }
 });
