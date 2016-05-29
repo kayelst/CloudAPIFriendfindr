e0b1d580f916c66ef355946ba7cf9de44a44ece1
MikeMeyers2504 <mike_sloefke@hotmail.com>
2016-05-30 00:05:09 +0200

    layout

--- client/templates/_partials/players/players.js
+++ client/templates/_partials/players/players.js
@@ -1,6 +1,5 @@
 Template.homePlayers.helpers({
   'languageCheck': function (value) {
-    console.log(value);
     if (!value || !Session.get("Language")) return true;
     return value.toLowerCase() === Session.get("Language").toLowerCase();
   }
@@ -8,7 +7,6 @@ Template.homePlayers.helpers({
 
 Template.homePlayers.helpers({
   'microphoneCheck': function (value) {
-    console.log(value);
     if (value == undefined || Session.get("Microphone") == undefined) return true;
     console.log(Session.get("Microphone") + " | "  + value + " : " + (value == Session.get("Microphone")));
     return value == Session.get("Microphone");
@@ -17,7 +15,6 @@ Template.homePlayers.helpers({
 
 Template.homePlayers.helpers({
   'serverCheck': function (value) {
-    console.log(value);
     if (!value || !Session.get("Server")) return true;
     return value.toLowerCase() === Session.get("Server").toLowerCase();
   }
@@ -25,7 +22,6 @@ Template.homePlayers.helpers({
 
 Template.homePlayers.helpers({
   'roleCheck': function (value) {
-    console.log(value);
     if (!value || !Session.get("Role")) return true;
     return value.toLowerCase() === Session.get("Role").toLowerCase();
   }
@@ -33,7 +29,6 @@ Template.homePlayers.helpers({
 
 Template.homePlayers.helpers({
   'nameCheck': function (value) {
-    console.log(value);
     if (!value || !Session.get("Name")) return true;
     return value.toLowerCase() === Session.get("Name").toLowerCase();
   }
